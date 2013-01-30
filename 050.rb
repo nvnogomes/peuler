f1482d8d0b6021a906d52798f933a5739bf5ea42
nvno <nvnogomes@gmail.com>
2013-01-30 00:36:20 +0000

    problem 50 solved

--- 050.rb
+++ 050.rb
@@ -1,39 +1,69 @@
 require "mathn"
 
 # Which prime, below one-million, can be written as the sum of the most consecutive primes?
+# 997651
+
+=begin
+solução proposta
+encontrar todos os números primos até que a soma seja inferior a 1M.
+somar todas as sequencias a partir dos numeros primos
+=end
 
 class Euler50
 
-	
-	def initialize(number=1000)
+	@prime_arr
+	@limit
+
+	def initialize(number=1000000)
 		@limit = number
-	end
-	
-	
-	def solve
-		factorial_size = 0
-		biggest_factorial = 0
-		Prime.each(@limit) do |current_prime|
-			sum_primes = count_prime_sum(current_prime)
-			puts "#{current_prime} #{sum_primes}\n"
-			if( sum_primes > factorial_size )
-				factorial_size = sum_primes
-				biggest_factorial = current_prime
-			end
-		end
-		puts "#{biggest_factorial} #{factorial_size}"
-		biggest_factorial
+		@prime_arr = Array.new
+
 	end
 
-	def count_prime_sum(n)
-		sum = 0
-		counter = 0
-		Prime.each(n) do |prime|
-			counter += 1
-			sum += prime
-			return counter if sum == n
-			return -1 if sum > n
+
+	def solve
+		biggest = 0
+		max = get_primes
+
+		i = 0
+		loop do
+
+			sum = @prime_arr.at(i)
+			j = i+1
+
+			loop do
+				sum += @prime_arr.at(j)
+
+				if sum.prime? && sum > biggest then
+						biggest = sum
+						puts "> #{biggest} #{j-i}"
+				end
+
+				j += 1
+				break if j == max || sum > @limit
+			end
+
+			i += 1
+			break if (i+1) == max
 		end
+
+		biggest
 	end
+
+
+	def get_primes
+		sum = 0
+
+		Prime.each(@limit) do |current_prime|
+
+			@prime_arr << current_prime
+			sum += current_prime
+
+			break if sum > @limit
+
+		end
+		@prime_arr.length
+	end
+
 
 end
